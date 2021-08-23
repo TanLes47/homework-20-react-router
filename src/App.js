9ef6a48090743ef51053db3de62a1eba77269cbb
Tetyana <tetyana.lesivtsiv@gmail.com>
2021-08-23 18:08:54 +0300

    Added src/components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import HomePage from './components/HomePage';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+        <Switch>
+          <Route exact path={'/'} component={HomePage} />
+				</Switch>
+		  </div>
+	)
 }
 
 export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import '../components/style';
+
+
+const HomePage = () => {
+    return <h1>Welcome to Venice</h1>
+}
+
+export default HomePage;
